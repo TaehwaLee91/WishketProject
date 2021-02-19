1e1c853cf4a612cb106f911d6b61588db59e82df
a7875 <a787574@naver.com>
2021-02-20 04:03:48 +0900

    회원가입(signup) -> 회원가입완료(signok) DB연동 v1 + 정규표현식 / 아이디,이메일 중복확인 기능 추가 + 위의 기능 불만족시 회원가입완료 페이지로 안넘어가게 설정

--- src/main/java/wishket/spring/mvc/dao/MemberDAO.java
+++ src/main/java/wishket/spring/mvc/dao/MemberDAO.java
@@ -4,4 +4,8 @@ import wishket.spring.mvc.vo.MemberVO;
 
 public interface MemberDAO {
     int insertMember(MemberVO mvo);
+
+    int selectOneUserid(String userid);
+
+    int selectOneEmail(String email);
 }
--- src/main/java/wishket/spring/mvc/service/MemberService.java
+++ src/main/java/wishket/spring/mvc/service/MemberService.java
@@ -5,4 +5,7 @@ import wishket.spring.mvc.vo.MemberVO;
 
 public interface MemberService {
     String newMember (MemberVO mvo);
+    String checkUserid(String userid);
+
+    String checkEmail(String email);
 }
