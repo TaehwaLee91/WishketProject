0571b043e662195fd4784c95415a8b782eef530d
retar40 <retar40@gmail.com>
2021-03-09 13:17:21 +0900

    21/03/09 프로젝트 찾기 - 필터항목 검색버튼 추가

--- src/main/java/wishket/spring/mvc/controller/BoardController.java
+++ src/main/java/wishket/spring/mvc/controller/BoardController.java
@@ -3,6 +3,7 @@ package wishket.spring.mvc.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import wishket.spring.mvc.service.ProjectService;
 import wishket.spring.mvc.vo.ProjectVO;
@@ -32,8 +33,8 @@ public class BoardController {
         return mv;
     }
 
-    @GetMapping("/project/board/filter")
-    public ModelAndView boardFilterView(ModelAndView mv){
+    @PostMapping("/project/list/filter")
+    public ModelAndView filterBoard(ModelAndView mv){
         mv.setViewName("projectboard/boardView.tiles");
         return mv;
     }
